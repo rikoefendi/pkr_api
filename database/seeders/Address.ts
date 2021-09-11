4e54a64b0682ee79d5e33314c601764381e4e16e
rikoefendi <rikoefendi100@gmail.com>
2021-09-11 18:59:31 +0700

    seed Address drop

--- database/seeders/Address.ts
+++ database/seeders/Address.ts
@@ -4,6 +4,7 @@ import {Address as AddressData, CityContract} from 'nik-validate'
 export default class AddressSeeder extends BaseSeeder {
 	public async run() {
 		// Write your database queries inside the run method
+		await Address.collection.drop()
 		const a = [...AddressData.provinces, ...AddressData.cities]
 		await Promise.all(a.map(async (address: CityContract) => {
 			await Address.create({
